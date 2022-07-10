518c161ad41e97dee7614744e9cf0a691e6c9e06
halm16701774 <78166203+halm16701774@users.noreply.github.com>
2022-07-10 21:36:18 +0700

    Add files via upload

---
+++ direkcurut.js
@@ -0,0 +1,13 @@
+type='text/javascript'>
+//<![CDATA[
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://cudgeletc.com/buihfc1e9n?key=0cbe24e609f9d9335a93f1b906b29028','_blank');
+	});
+//]]>
